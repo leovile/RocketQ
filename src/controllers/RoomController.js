61e6dbaaf80bd5c19174773ae9f7171ff5ce6858
Leonardo Vilela <vilela.leoo@gmail.com>
2021-06-28 12:46:04 -0300

    First commit

---
+++ src/controllers/RoomController.js
@@ -0,0 +1,56 @@
+const Database = require('../db/config')
+
+
+module.exports = {
+  async create(req, res){
+    const db = await Database()
+    const pass = req.body.password
+    let roomId = ''
+    let isRoom = true
+
+
+    while(isRoom){
+      for(var i = 0; i < 6; i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+        roomId += Math.floor(Math.random()*10).toString()
+      }
+      const roomsExistIds = await db.all(`SELECT * FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+      if(!isRoom){
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+        ) VALUES (
+          ${parseInt(roomId)},
+          ${pass}
+        )`)
+      }
+    }
+
+    await db.close()
+
+    res.redirect(`/room/${roomId}`)
+  },
+
+  async open(req, res){
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+    let isNoQuestions
+
+    if(questions.lenght == 0){
+      if(questionRead.lenght == 0){
+        isNoQuestions = true
+      }
+    }
+
+    res.render("room", {roomId: roomId, questions: questions, questionRead: questionRead, isNoQuestions: isNoQuestions})
+  },
+
+  enter(req, res){
+    const roomId = req.body.roomId
+
+    res.redirect(`/room/${roomId}`)
+  }
+}
